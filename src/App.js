055d97ab59c1a37516e59ba6e9fddcdad2cd7d3f
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-03-01 14:42:45 -0600

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Cynthia V. Johnsen </h1>
+        <NavBar />
+      </header>
+
+      <div className="Body">
+        <Switch>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +37,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }

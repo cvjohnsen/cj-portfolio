ceda0d8ef87719f0ce6ac41efec74c39ba8c7225
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-03-04 13:34:02 -0600

    10th commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,13 +4,21 @@ const Projects = () => {
     <div style={box1}>
       <div>
         <h1>Javascript Project</h1>
-        <a href="https://dazzling-kare-5bcc4b.netlify.app/" target="_blank">
+        <a
+          href="https://dazzling-kare-5bcc4b.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={game} alt="memoryGame" width="500px" />
         </a>
       </div>
       <div>
         <h1>React Project</h1>
-        <a href="https://dazzling-kare-5bcc4b.netlify.app/" target="_blank">
+        <a
+          href="https://dazzling-kare-5bcc4b.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={game} alt="memoryGame" width="500px" />
         </a>
       </div>
